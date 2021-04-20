19624912c280b45f236ec7e0e8e8a5d82cd81f47
SoushiYamamoto <48818145+SoushiYamamoto@users.noreply.github.com>
2021-04-21 02:15:07 +0900

    Update fan.py

--- fan.py
+++ fan.py
@@ -19,7 +19,7 @@ def main():
     print(res_cmd_no_lfeed(cmdFan))
 
     for i, gpu in enumerate(gpus):
-        if (int(gpu) > 60):
+        if (int(gpu) > 65):
             print('GPU'+str(i)+' - TOO HOT!')
             targetFanNum = i * 2
             targetFanSpeed = int(max(min(100,round(int(fans[i])+10, -1)),40))
@@ -31,7 +31,7 @@ def main():
             result=res_cmd_no_lfeed(cmdFanChange1)
             cmdFanChange2 = ("DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 nvidia-settings -a [fan:"+str(targetFanNum+1)+"]/GPUTargetFanSpeed="+str(targetFanSpeed))
             result=res_cmd_no_lfeed(cmdFanChange2)
-        elif (int(gpu) < 55):
+        elif (int(gpu) < 60):
             print('GPU'+str(i)+' - fan speed can be decreased.')
             targetFanNum = i * 2
             targetFanSpeed = int(max(min(100,round(int(fans[i])-10, -1)),40))
