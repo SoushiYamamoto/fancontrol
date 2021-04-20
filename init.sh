ba046801299aff9515d3a0b98b53fd7411b9858a
SoushiYamamoto <48818145+SoushiYamamoto@users.noreply.github.com>
2021-04-20 22:16:10 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@ cd /
 rm f /cron_fan
 rm f /etc/cron.d/cron_fan
 rm f /fan.py
-wget https://github.com/SoushiYamamoto/fancontrol/blob/main/cron_fan
-wget https://github.com/SoushiYamamoto/fancontrol/blob/main/fan.py
+wget https://raw.githubusercontent.com/SoushiYamamoto/fancontrol/main/cron_fan
+wget https://raw.githubusercontent.com/SoushiYamamoto/fancontrol/main/fan.py
 mv /cron_fan /etc/cron.d/
 service cron restart
